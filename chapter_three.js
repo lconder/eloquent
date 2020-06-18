b1d3cfd3c6ffef31c1f4fdd417303390f517b6ca
Luis Conde <lconder@MacBookPro.local>
2020-06-18 15:16:28 -0500

    Chapter 3 second part

--- chapter_three.js
+++ chapter_three.js
@@ -82,6 +82,126 @@ console.log(reverseArray(arr));
 console.log(reverseArrayInPlace(arr));
 
 
+/**
+A list
+Objects, as generic blobs of values, can be used to build all sorts of data structures. A common data structure is the list (not to be confused with array). A
+list is a nested set of objects, with the first object holding a reference to the
+second, the second to the third, and so on.
+
+let list = {
+	value: 1,
+	rest: {
+		value: 2,
+		rest: {
+			value: 3,
+			rest: null
+		} 
+	}
+};
+
+A nice thing about lists is that they can share parts of their structure. For
+example, if I create two new values {value: 0, rest: list} and {value: -1,
+rest: list} (with list referring to the binding defined earlier), they are both
+independent lists, but they share the structure that makes up their last three
+elements. The original list is also still a valid three-element list.
+Write a function arrayToList that builds up a list structure like the one
+shown when given [1, 2, 3] as argument. Also write a listToArray function
+that produces an array from a list. Then add a helper function prepend, which
+takes an element and a list and creates a new list that adds the element to the
+front of the input list, and nth, which takes a list and a number and returns
+the element at the given position in the list (with zero referring to the first
+element) or undefined when there is no such element.
+If you haven’t already, also write a recursive version of nth.
+*/
+
+console.log("\nA list:");
+
+let array = [1, 2, 3, 4];
+
+let list = {
+	value: 1,
+	rest: {
+		value: 2,
+		rest: {
+			value: 3,
+			rest: {
+				value: 4,
+				rest: null
+			}
+		}
+	}
+};
+
+const arrayToList = arr => {
+
+	let list = null;
+
+	for (let i = arr.length - 1; i >= 0; i--) {
+		
+		list = {
+			value: arr[i],
+			rest: list
+		}
+
+	}
+
+	return list;
+
+}
+
+
+const listToArray = list => {
+
+	let arr = [];
+	let nodus = list;
+
+	while(nodus) {
+
+		arr.push(nodus.value);
+		nodus = nodus.rest;
+	}
+
+	return arr;
+}
+
+const prepend = (list, element) => ({
+		value: element,
+		rest: list
+});
+
+const nth = (list, value) => {
+
+	let i=0, index;
+	let nodus = list;
+
+	while(nodus) {
+
+		if(nodus.value === value) {
+
+			index = i;
+			break;
+
+		}
+
+		i++;
+		nodus = nodus.rest;
+	}
+
+	return index;
+
+}
+
+console.log(JSON.stringify(arrayToList(array)));
+console.log(listToArray(list));
+
+list = prepend(list, 0);
+list = prepend(list, 10);
+
+console.log(JSON.stringify(list));
+
+console.log(nth(list, -1));
+console.log(nth(list, 0));
+
 
 
 
